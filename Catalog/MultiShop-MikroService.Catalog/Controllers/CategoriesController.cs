88026c3f4652345e8ce3e4cf724989bb2f67a9b8
Omer Polat <o.polat.tr@outlook.com>
2024-10-18 00:07:43 +0300

    Catalog konfigürasyonları ve controllerlar

---
+++ Catalog/MultiShop-MikroService.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop_MikroService.Catalog.Dtos.CategoryDtos;
+using MultiShop_MikroService.Catalog.Services.CategoryServices;
+
+namespace MultiShop_MikroService.Catalog.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private readonly ICategoryService _categoryService;
+
+		public CategoriesController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> CategoryList()
+		{
+			var values = _categoryService.GetAllCategoryAsync();
+			return Ok(values);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetCategoryById(string id)
+		{
+			var values = _categoryService.GetByIdCategoryAsync(id);
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+		{
+			await _categoryService.CreateCategoryAsync(createCategoryDto);
+			return Ok("kategori başarı ile eklendi");
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> DeleteCategory(string id)
+		{
+			await _categoryService.DeleteCategoryAsync(id);
+			return Ok("Kategori başarı ile silindi");
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+		{
+			await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+			return Ok("Kategori Güncellendi");
+		}
+	}
+}
--- Catalog/MultiShop-MikroService.Catalog/Program.cs
+++ Catalog/MultiShop-MikroService.Catalog/Program.cs
@@ -1,4 +1,25 @@
+using Microsoft.Extensions.Options;
+using MultiShop_MikroService.Catalog.Services.CategoryServices;
+using MultiShop_MikroService.Catalog.Services.ProductDetailServices;
+using MultiShop_MikroService.Catalog.Services.ProductImageServices;
+using MultiShop_MikroService.Catalog.Services.ProductServices;
+using MultiShop_MikroService.Catalog.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+	return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 // Add services to the container.
 
