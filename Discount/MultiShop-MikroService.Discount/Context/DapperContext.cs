82420784838d314ba3bce20b71d6f834a7e37415
Omer Polat <o.polat.tr@outlook.com>
2024-10-18 03:37:21 +0300

    Discount Service and Order Service

---
+++ Discount/MultiShop-MikroService.Discount/Context/DapperContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop_MikroService.Discount.Entities;
+using System.Data;
+
+namespace MultiShop_MikroService.Discount.Context
+{
+	public class DapperContext :DbContext
+	{
+		private readonly IConfiguration _configuration;
+		private readonly string _connectionString;
+		public DapperContext(IConfiguration configuration)
+		{
+			_configuration = configuration;
+			_connectionString = _configuration.GetConnectionString("DefaultConnection");
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-IJ6DE2I\\SQLEXPRESS;initial Catalog=MultiShopDiscountDb;integrated Security=true");
+		}
+		public DbSet<Coupon> Coupons { get; set; }
+		public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+	}
+}
---
+++ Discount/MultiShop-MikroService.Discount/Services/DiscountService.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
+using MultiShop_MikroService.Discount.Context;
+using MultiShop_MikroService.Discount.Dtos;
+
+namespace MultiShop_MikroService.Discount.Services
+{
+	public class DiscountService : IDiscountService
+	{
+		private readonly DapperContext _dapperContext;
+
+		public DiscountService(DapperContext dapperContext)
+		{
+			_dapperContext = dapperContext;
+		}
+
+		public async Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto)
+		{
+			string query = "insert into Coupons (CouponCode,CouponRate,CouponIsActive,CouponValidDate) values (@couponCode,@couponRate,@couponIsActive,@couponValidDate)";
+			var parameters = new DynamicParameters();
+			parameters.Add("@couponCode", createCouponDto.CouponCode);
+			parameters.Add("@couponRate", createCouponDto.CouponRate);
+			parameters.Add("@couponIsActive", createCouponDto.CouponIsActive);
+			parameters.Add("@couponValidDate", createCouponDto.CouponValidDate);
+			using(var connection = _dapperContext.CreateConnection())
+			{
+				await connection.ExecuteAsync(query, parameters);
+			}
+		}
+
+		public async Task DeleteDiscountCouponAsync(int id)
+		{
+			string query = "Delete From Coupons where CouponId=@couponId";
+			var parameters = new DynamicParameters();
+			parameters.Add("couponId", id);
+			using (var connection = _dapperContext.CreateConnection())
+			{
+				await connection.ExecuteAsync(query, parameters);
+			}
+		}
+
+		public async Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponAsync()
+		{
+			string query = "Select * From Coupons";
+			using (var connection = _dapperContext.CreateConnection())
+			{
+				var values = await connection.QueryAsync<ResultDiscountCouponDto>(query);
+				return values.ToList();
+			}
+		}
+
+		public async Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id)
+		{
+			string query = "Select * From Coupons Where CouponId=@couponId";
+			var parameters = new DynamicParameters();
+			parameters.Add("@couponId", id);
+			using (var connection = _dapperContext.CreateConnection())
+			{
+				var values = await connection.QueryFirstOrDefaultAsync<GetByIdDiscountCouponDto>(query, parameters);
+				return values;
+			}
+		}
+
+		public async Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto)
+		{
+			string query = "Update Coupons Set CouponCode=@code,CouponRate=@rate,CouponIsActive=@isActive,CouponValidDate=@validDate where CouponId=@couponId";
+			var parameters = new DynamicParameters();
+			parameters.Add("@code", updateCouponDto.CouponCode);
+			parameters.Add("@rate", updateCouponDto.CouponRate);
+			parameters.Add("@isActive", updateCouponDto.CouponIsActive);
+			parameters.Add("@validDate", updateCouponDto.CouponValidDate);
+			parameters.Add("@couponId", updateCouponDto.CouponId);
+			using (var connection = _dapperContext.CreateConnection())
+			{
+				await connection.ExecuteAsync(query, parameters);
+			}
+		}
+	}
+}
