aa531b4c105be87de0628e6208a7711791b1513b
Omer Polat <o.polat.tr@outlook.com>
2024-10-20 22:18:09 +0300

    Service reqistiration in Program.cs

--- Services/Order/Presentation/MultiShop-MikroService.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop-MikroService.Order.WebApi/Program.cs
@@ -1,8 +1,15 @@
 using MultiShop_MikroService.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop_MikroService.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop_MikroService.Order.Application.Interfaces;
+using MultiShop_MikroService.Order.Application.Services;
+using MultiShop_MikroService.Order.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+#region
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -14,7 +21,7 @@ builder.Services.AddScoped<GetOrderDetailQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
-
+#endregion
 
 
 // Add services to the container.
